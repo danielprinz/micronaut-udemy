95e904cad7e1ffdc11f1a448fed30b93f5dda94f
danielprinz <daniel.prinz@outlook.com>
2021-07-20 21:35:54 +0200

    forward liquibase v4 logs to logger

--- mn-funding/src/main/java/com/danielprinz/udemy/Application.java
+++ mn-funding/src/main/java/com/danielprinz/udemy/Application.java
@@ -1,10 +1,14 @@
 package com.danielprinz.udemy;
 
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public class Application {
 
   public static void main(String[] args) {
+    // Bridge JUL to Slf4j
+    SLF4JBridgeHandler.removeHandlersForRootLogger();
+    SLF4JBridgeHandler.install();
     Micronaut.run(Application.class, args);
   }
 }
